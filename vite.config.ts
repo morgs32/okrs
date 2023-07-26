3dcfdcc847ecf607e8773ac0745b9f36591a724b
Morgan <morgan.intrator@gmail.com>
2023-07-26 10:31:16 -0400

    fix: uses bunchee

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
-// / <reference types="vitest" />
-import { defineConfig } from 'vite';
-
-const fileName = {
-  es: 'index.mjs',
-  cjs: 'index.cjs',
-};
-
-const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
+import { defineConfig } from 'vitest/config';
 
 module.exports = defineConfig({
   base: './',
-  build: {
-    emptyOutDir: false,
-    lib: {
-      entry: 'src/index.ts',
-      formats,
-      fileName: (format) => fileName[format],
-    },
-  },
   test: {
     include: ['**/*.test.ts'],
     globals: true
   },
-});
+})
