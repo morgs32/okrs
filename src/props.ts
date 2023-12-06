bc7534b709c3c0003143b73bb981b2087370625d
Morgan <morgan.intrator@gmail.com>
2023-12-06 09:59:09 -0700

    wip

--- src/props.ts
+++ src/props.ts
@@ -1,28 +1,22 @@
 import { handle } from './handle';
 import { Fail } from './Fail';
+import { isPromise } from 'util/types';
 
-export async function props<T extends { [k: string]: Promise<any> }>(props: T) {
+export async function props<T>(props: T) {
   const results = {} as {
     [K in keyof T]: T[K] extends Promise<infer R> ? R : never;
   };
   const promises: Promise<any>[] = [];
-  let fail: Fail | undefined;
   for (const key in props) {
-    promises.push(
-      // eslint-disable-next-line no-loop-func
-      props[key as keyof T].catch(handle).then((v) => {
-        if (v instanceof Fail && !fail) {
-          fail = v;
-        }
-        results[key] = v;
-      })
-    );
+    let value: any = props[key];
+    if (!isPromise(value)) {
+      continue;
+    }
+    const promise = value.catch(handle).then((v) => {
+      results[key] = v;
+    });
+    promises.push(promise);
   }
-  if (promises.length) {
-    await Promise.all(promises);
-  }
-  if (fail) {
-    throw fail;
-  }
+
   return results;
 }
