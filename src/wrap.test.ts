4327080811bc27f10dac19afc7126395498fa89d
Morgan <morgan.intrator@gmail.com>
2024-02-01 23:54:42 -0500

    fix: fixes okrs.wrap around async fns

--- src/wrap.test.ts
+++ src/wrap.test.ts
@@ -1,4 +1,6 @@
+import { assert, Equals } from 'tsafe';
 import { okrs } from '.';
+import { Either } from './types';
 
 describe('wrap', () => {
   it('works', async () => {
@@ -9,8 +11,23 @@ describe('wrap', () => {
       throw new Error('fail');
     });
     const kr = wrapped(1, 2);
+    assert<Equals<Either<number>, typeof kr>>();
     expect(kr.value).toBe(3);
 
     expect(wrapped(1, 1)).toMatchInlineSnapshot('[Error: fail]');
+  });
+
+  it('works async', async () => {
+    const wrapped = okrs.wrap(async (a: number, b: number) => {
+      if (a + b === 3) {
+        return a + b;
+      }
+      throw new Error('fail');
+    });
+    const kr = await wrapped(1, 2);
+    assert<Equals<Either<number>, typeof kr>>();
+    expect(kr.value).toBe(3);
+
+    expect(await wrapped(1, 1)).toMatchInlineSnapshot('[Error: fail]');
   });
 });
--- src/wrap.ts
+++ src/wrap.ts
@@ -2,7 +2,11 @@ import { okrs } from '.';
 import { Either } from './types';
 
 export function wrap<F extends (...args: any[]) => any>(fn: F) {
-  return function wrapped(...args: Parameters<F>): Either<ReturnType<F>> {
-    return okrs.coerce(() => fn(...args), ...args) as Either<ReturnType<F>>;
+  return function wrapped(
+    ...args: Parameters<F>
+  ): ReturnType<F> extends Promise<infer A>
+    ? Promise<Either<A>>
+    : Either<ReturnType<F>> {
+    return okrs.coerce(() => fn(...args), ...args) as any;
   };
 }
