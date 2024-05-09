083bde03d8fab36eac6fe77638811c7baa565872
Morgan <morgan.intrator@gmail.com>
2024-05-09 11:47:44 -0500

    wip: add count to batch

--- src/batch.ts
+++ src/batch.ts
@@ -2,16 +2,18 @@ import { okrs } from "."
 
 export async function batch<T, R>(
   items: Array<T>,
-  fn: (item: T) => Promise<R>,
+  fn: (item: T, count: number) => Promise<R>,
   size: number = 100,
 ): Promise<R[]> {
+  let count = 0
   const batches: Array<Array<R>> = []
   const remaining = [...items]
   while (remaining.length > 0) {
     let batch: Array<Promise<any>> = []
     while (batch.length < size && remaining.length > 0) {
       const v = remaining.shift()!
-      batch.push(fn(v))
+      batch.push(fn(v, count))
+      count++
     }
     batches.push(await okrs.all(batch))
   }
